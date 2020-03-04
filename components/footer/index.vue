e91929393d128f5917587d787754a4212949571a
xiaotian <952822399@qq.com>
2020-03-04 22:38:49 +0800

    dao jam home,
    简单的做了一些 适配, 增加侧边栏和简易的动画效果

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,280 +1,186 @@
 <template>
   <footer class="footer">
-    <div class="footer-content">
-      <div class="content footer-c6">
-        <div class="nav">
-          <a class="href" target="_blank" href="https://www.yuque.com/matataki">
-            {{ $t('footer.helpAndSupport') }}
-          </a>
-          <a class="href" target="_blank" href="https://www.yuque.com/matataki/matataki">
-            {{ $t('footer.hellpbook') }}
-          </a>
-          <a class="href" target="_blank" href="https://www.matataki.io/p/781">
-            {{ $t('footer.point') }}
-          </a>
-          <!-- 隐私政策与用户协议 -->
-          <!-- 去掉链接 -->
-          <a class="href" target="_blank">
-            {{ $t('footer.privacyPolicy') }}
-          </a>
-          <a class="href" target="_blank">
-            {{ $t('footer.userAgreement') }}
-          </a>
-          <!-- 保留链接 -->
-          <!-- <a class="href" target="_blank" href="/policy">
-            {{ $t('footer.privacyPolicy') }}
-          </a>
-          <a class="href" target="_blank" href="/agreement">
-            {{ $t('footer.userAgreement') }}
-          </a> -->
-          <a class="href" target="_blank" href="https://github.com/Matataki-io/Matataki-FE">
-            {{ $t('footer.about') }}
-          </a>
-          <a class="href" target="_blank" href="https://www.yuque.com/matataki/matataki_version">
-            {{ $t('footer.updateRecord') }}
-          </a>
-          <a class="href" target="_blank" href="http://www.matataki.io/p/616">
-            {{ $t('footer.billboard') }}
-          </a>
-        </div>
-        <p class="link">
-          {{ $t('footer.link') }}:
-          <a href="https://matters.news/" target="_blank">
-            Matters
-          </a>
-          |
-          <a href="https://maskbook.com/" target="_blank">
-            Maskbook
-          </a>
-          |
-          <a href="https://orange.xyz/" target="_blank">
-            {{ $t('footer.orange') }}
-          </a>
-          |
-          <a href="https://www.defi.wtf" target="_blank">
-            DeFi.wtf
-          </a>
-        </p>
-      </div>
-      <div class="footer-c4">
-        <div>
-<!--          Removed Matataki Logo-->
-<!--          <img class="logo" src="@/assets/img/daodelion.png" alt="logo">-->
-          <div class="icon wechat-icon">
-            <div class="wechat">
-              <div class="wechat-img">
-                <img src="@/assets/img/wechat-group.png" alt="code">
-                <p class="wechat-title">
-                  {{ $t('footer.scanCode') }}
-                </p>
-              </div>
-              <div class="wechat-img">
-                <img src="@/assets/img/wechat-public.png" alt="code">
-                <p class="wechat-title">
-                  {{ $t('footer.follow') }}
-                </p>
-              </div>
-            </div>
-          </div>
-          <a target="_blank" href="https://github.com/Matataki-io/Matataki-FE">
-            <div class="icon github-icon" />
-          </a>
-          <a target="_blank" href="https://t.me/smartsignature_io">
-            <div class="icon telegram-icon" />
-          </a>
-          <a target="_blank" href="https://twitter.com/realmatataki">
-            <svg-icon class="icon twitter-icon" icon-class="twitter" />
-          </a>
-          <a target="_blank" href="https://www.facebook.com/MatatakiOfficial/">
-            <div class="icon facebook-icon" />
-          </a>
-        </div>
-        <p class="copyright">
-          Copyright © 2018-2019 ANDOROMEDA TECH.ltd
-        </p>
+    <div class="footer-social">
+      <div class="footer-social__block" :class="item.name" v-for="(item, index) in socialList" :key="index">
+        <svg-icon :icon-class="item.icon" class="icon" />
       </div>
     </div>
+    <p class="footer-copyright">Copyright © DAO JAM</p>
   </footer>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      socialList: [
+        {
+          name: 'weixin',
+          icon: 'weixin'
+        },
+                {
+          name: 'weibo',
+          icon: 'weibo'
+        },
+        {
+          name: 'twitter',
+          icon: 'twitter'
+        },
+        {
+          name: 'facebook',
+          icon: 'fb'
+        }
+      ]
+    }
+  }
+}
 </script>
+
 
 <style lang="less" scoped>
 .footer {
-    height: 180px;
-    background: #333333;
-    padding-top: 52px;
-    padding-bottom: 52px;
-    box-sizing: border-box;
-  &-content {
-    max-width: 1200px;
-    height: 100%;
-    margin: 0 auto;
-    padding: 0 10px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    box-sizing: border-box;
-  }
-  .footer-c6 {
-    width: 60%;
-  }
-  .footer-c4 {
-    width: 40%;
-  }
+  background: #555555;
+  padding: 40px 20px;
+  text-align: center;
+  box-sizing: border-box;
 }
-
-.logo {
-  width: 178px;
+.footer-social {
+  margin-bottom: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
-.content {
-  .nav {
-    padding: 0;
-    margin: 0;
-    display: flex;
-    align-items: center;
-    flex-wrap: wrap;
-    .href {
-      margin-right: 20px;
-      text-decoration: none;
-      font-size:14px;
-      color:rgba(178,178,178,1);
-      line-height:22px;
-      &:hover {
-        color: #fff;
-      }
-    }
-
+.footer-social__block {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+  overflow: hidden;
+  color: #fff;
+  float: left;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: 40px;
+  transition: all .3s;
+  &:nth-child(1) {
+    margin-left: 0;
   }
-
-}
-
-.icon {
-  margin-left: 22px;
-  display: inline-block;
-  cursor: pointer;
-  .wechat {
-    display: none;
-    justify-content: space-between;
-    align-items: center;
-    position: absolute;
-    top: -200px;
-    left: -146px;
-    width: 310px;
-    background: #fff;
-    border-radius: 6px;
-    padding: 14px;
-    box-sizing: border-box;
-    box-shadow: 0 0 10px rgba(0,0,0,.1);
-    &-img {
-      text-align: center;
-    }
-    &-title {
-      padding: 0;
-      margin: 0;
-      font-size: 14px;
-      line-height: 1.5;
-      color: #000;
-    }
-    &::after {
-      position: absolute;
-      bottom: -20px;
-      left: 50%;
-      transform: translate(-50%, 0);
-      display: block;
-      content: '';
-      width: 0;
-      height: 0;
-      border: 10px solid #fff;
-      border-color: #fff transparent transparent;
-    }
-    img {
-      border: 1px solid #eee;
-      width: 130px;
-      box-sizing: border-box;
-    }
+  .icon {
+    font-size: 34px;
   }
-  &:hover .wechat {
-    display: flex;
-  }
-  &.wechat-icon {
-    width: 28px;
-    height: 23px;
-    position: relative;
-    background-image: url('../../assets/img/footer_wechat.svg');
+  &.weixin {
+    background: #00C800;
     &:hover {
-      background-image: url('../../assets/img/footer_wechat_active.svg');
+      background: mix(#00C800, #fff, 90%);
     }
   }
-  &.github-icon {
-    width: 22px;
-    height: 24px;
-    background-image: url('../../assets/img/footer_github.svg');
+  &.weibo {
+    background: #EA5D5C;
     &:hover {
-      background-image: url('../../assets/img/footer_github_active.svg');
+      background: mix(#EA5D5C, #fff, 90%);
     }
   }
-  &.telegram-icon{
-    width: 25px;
-    height: 21px;
-    background-image: url('../../assets/img/footer_telegram.svg');
+  &.twitter {
+    background: #78CBEF;
     &:hover {
-      background-image: url('../../assets/img/footer_telegram_active.svg');
+      background: mix(#78CBEF, #fff, 90%);
     }
   }
-  &.twitter-icon{
-    width: 24px;
-    height: 24px;
-    color: #B2B2B2;
+  &.facebook {
+    background: #3C599B;
     &:hover {
-      color: #00ACED;
-    }
-  }
-  &.facebook-icon{
-    width: 24px;
-    height: 24px;
-    background-image: url('../../assets/img/footer_facebook.svg');
-    &:hover {
-      background-image: url('../../assets/img/footer_facebook_active.svg');
+      background: mix(#3C599B, #fff, 90%);
     }
   }
 }
-.copyright {
-  font-size: 14px;
-  color: rgba(178, 178, 178, 1);
-  line-height: 22px;
-  margin: 10px 0 0;
-}
-
-.link {
-  margin: 10px 0 0;
-  font-size:14px;
-  color:rgba(178,178,178,1);
+.footer-copyright {
+  text-align: center;
+  padding: 0;
+  margin: 0;
+  font-size:16px;
+  font-weight:400;
+  color:rgba(255,255,255,1);
   line-height:22px;
-  a {
-    color:rgba(178,178,178,1);
-    text-decoration: none;
-    margin: 0 4px;
-    &:nth-child(1) {
-      margin-left: 12px;
+}
+
+
+
+@media screen and (max-width: 520px) {
+  .footer {
+    padding: 10px 20px;
+  }
+  .footer-social {
+    margin-bottom: 10px;
+  }
+  .footer-social__block {
+    width: 30px;
+    height: 30px;
+    margin-left: 10px;
+    .icon {
+      font-size: 16px;
     }
-    &:hover {
-      color: #fff;
+  }
+  .footer-copyright {
+    font-size:12px;
+  }
+}
+
+@media screen and (min-width: 520px) {
+  .footer {
+    padding: 20px 20px;
+  }
+  .footer-social {
+    margin-bottom: 20px;
+  }
+  .footer-social__block {
+    width: 40px;
+    height: 40px;
+    margin-left: 20px;
+    .icon {
+      font-size: 20px;
+    }
+  }
+  .footer-copyright {
+    font-size:14px;
+  }
+}
+
+@media screen and (min-width: 992px) {
+  .footer {
+    padding: 30px 20px;
+  }
+  .footer-social {
+    margin-bottom: 30px;
+  }
+  .footer-social__block {
+    width: 40px;
+    height: 40px;
+    margin-left: 30px;
+    .icon {
+      font-size: 22px;
     }
   }
 }
 
-@media (max-width: 900px) {
+@media screen and (min-width: 1920px) {
   .footer {
-    height: auto;
+    padding: 40px 20px;
   }
-  .footer-content {
-    flex-direction: column;
-    .content {
-      margin-bottom: 20px;
+  .footer-social {
+    margin-bottom: 40px;
+  }
+  .footer-social__block {
+    width: 60px;
+    height: 60px;
+    .icon {
+      font-size: 34px;
     }
   }
+  .footer-copyright {
+    font-size:16px;
+  }
 }
+
+
+
 </style>
