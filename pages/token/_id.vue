c5812b36ade122156ece3ec16a9a5f720947b9a3
xiaotian <952822399@qq.com>
2020-03-09 18:57:12 +0800

    token page

--- pages/token/_id.vue
+++ pages/token/_id.vue
@@ -1,93 +1,173 @@
 <template>
-  <layout v-model="tabPage" :minetoken-token="minetokenToken" :minetoken-user="minetokenUser" :minetoken-exchange="minetokenExchange">
-    <tokenFanCoins v-if="tabPage === 0" />
-    <tokenFanCoinsDetail v-if="tabPage === 1" />
-    <tokenLiquidity v-if="tabPage === 2" />
-    <tokenLiquidityDetail v-if="tabPage === 3" :token="minetokenToken" />
-  </layout>
+  <div class="token">
+    <g-header></g-header>
+    <div class="token-main">
+      <div class="token-head">
+        <div class="head-user">
+          <avatar></avatar>
+          <div class="head-user__info">
+            <h2>XIOO</h2>
+            <div class="dao__info__number">
+              <div class="dao__info__number__block">
+                <svg-icon icon-class="members" class="icon"></svg-icon>
+                {{ 0 }}
+              </div>
+              <div class="dao__info__number__block">
+                <svg-icon icon-class="daos" class="icon"></svg-icon>
+                {{ 0 }}
+              </div>
+              <div class="dao__info__number__block">
+                <svg-icon icon-class="tickets" class="icon"></svg-icon>
+                {{ 0 }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <span class="head-amount">
+          I have: 1000 G & 10234 T
+        </span>
+
+      </div>
+
+
+      <div class="token-list">
+        <h2 class="token-title">Brief Introduction</h2>
+        <p class="token-brief">
+          And when I prophesied, Pelatiah the son of Benaiah died; and I fell on my face and cried out with a loud voice, and I said, Ah, Lord Jehovah! Are You making a full end to the remnant of Israel?And when I prophesied, Pelatiah the son of Benaiah died; and I fell on my face and cried out with a loud voice, and I said, Ah, Lord Jehovah! Are You making a full end to the remnant of Israel?And when I prophesied, Pelatiah the son of Benaiah died; and I fell on my face and cried out with a loud voice, and I said, Ah, Lord Jehovah! 
+        </p>
+      </div>
+
+      <div class="token-list">
+        <h2 class="token-title">Achievement</h2>
+        <!-- todo -->
+      </div>
+
+      <div class="token-list">
+        <h2 class="token-title">DAO Contract Address</h2>
+        <!-- todo -->
+        cdowvnfodrenveiornvoinreovoerinvfdlksmvpwdfopvmpodfsksvcmpdow
+      </div>
+
+      <div class="token-list">
+        <h2 class="token-title">Members</h2>
+        <!-- todo -->
+      </div>
+
+
+    </div>
+  </div>
 </template>
 
 <script>
-// import userPagination from '@/components/user/user_pagination.vue'
-// import minetokenCard from '@/components/user/minetoken_card'
-import layout from '@/components/token/token_layout.vue'
-import tokenFanCoins from '@/components/token/token_fan_coins'
-import tokenFanCoinsDetail from '@/components/token/token_fan_coins_detail'
-import tokenLiquidity from '@/components/token/token_liquidity'
-import tokenLiquidityDetail from '@/components/token/token_liquidity_detail'
-import { extractChar } from '@/utils/reg'
+import avatar from '@/common/components/avatar/index.vue'
 
 export default {
   components: {
-    layout,
-    tokenFanCoins,
-    tokenFanCoinsDetail,
-    tokenLiquidity,
-    tokenLiquidityDetail
-  },
-  head() {
-    return {
-      title: `${this.minetokenToken.symbol}-${this.minetokenToken.name}`,
-      meta: [
-        { hid: 'description', name: 'description', content: `${this.minetokenToken.brief}` || `${this.minetokenToken.symbol}-${this.minetokenToken.name}` },
-        /* <!--  Meta for Twitter Card --> */
-        { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
-        { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '@Andoromeda' },
-        { hid: 'twitter:title', name: 'twitter:title', property: 'twitter:title', content: `${this.minetokenToken.symbol}-${this.minetokenToken.name}` },
-        { hid: 'twitter:description', name: 'description', property: 'twitter:description', content: `${this.minetokenToken.brief}` || `${this.minetokenToken.symbol}-${this.minetokenToken.name}` },
-        { hid: 'twitter:url', name: 'twitter:url', property: 'twitter:url', content: `${process.env.VUE_APP_PC_URL}/token/${this.$route.params.id}` },
-        { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: this.$API.getImg(this.minetokenToken.logo) },
-        /* <!--  Meta for OpenGraph --> */
-        { hid: 'og:site_name', name: 'og:site_name', property: 'og:site_name', content: '瞬MATATAKI' },
-        { hid: 'og:title', name: 'og:title', property: 'og:title', content: `${this.minetokenToken.symbol}-${this.minetokenToken.name}` },
-        { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article' },
-        { hid: 'og:url', name: 'og:url', property: 'og:url', content: `${process.env.VUE_APP_PC_URL}/token/${this.$route.params.id}` },
-        { hid: 'og:image', name: 'og:image', property: 'og:image', content: this.$API.getImg(this.minetokenToken.logo) },
-        { hid: 'og:description', name: 'description', property: 'og:description', content: `${this.minetokenToken.brief}` || `${this.minetokenToken.symbol}-${this.minetokenToken.name}` }
-        /* end */
-      ],
-      link: [
-        { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.5.1/katex.min.css' }
-      ]
-    }
-  },
-  data() {
-    return {
-      tabPage: Number(this.$route.query.tab) || 0,
-      minetokenToken: Object.create(null),
-      minetokenUser: Object.create(null),
-      minetokenExchange: Object.create(null)
-    }
-  },
-  async asyncData({ $axios, route, req }) {
-    // 获取cookie token
-    let accessToekn = ''
-    // 请检查您是否在服务器端
-    if (process.server) {
-      const cookie = req && req.headers.cookie ? req.headers.cookie : ''
-      const token = extractChar(cookie, 'ACCESS_TOKEN=', ';')
-      accessToekn = token ? token[0] : ''
-    }
-    const res = await $axios({
-      url: `/minetoken/${route.params.id}`,
-      methods: 'get',
-      headers: { 'x-access-token': accessToekn }
-    })
-
-    if (res.code === 0) {
-      return {
-        minetokenToken: res.data.token || Object.create(null),
-        minetokenUser: res.data.user || Object.create(null),
-        minetokenExchange: res.data.exchange || Object.create(null)
-      }
-    } else {
-      console.error(res.message)
-    }
-  },
-  methods: {
-  }
+    avatar
+  }  
 }
 </script>
 
-<style scoped>
+<style lang="less" scoped>
+// common
+.token-list {
+  margin: 40px 0 0 0;
+}
+.token-title {
+  font-size:24px;
+  font-weight:500;
+  color:rgba(255,255,255,1);
+  line-height:33px;
+  padding: 0;
+  margin: 0;
+}
+// common end
+
+
+.token {
+  padding: 60px 0 0 0;
+  min-height: calc(100% - (60px + 200px));
+  background-color: #0E2144;
+}
+.token-main {
+  max-width: 1200px;
+  padding: 0 20px;
+  margin: 0 auto;
+}
+
+
+.token-head {
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-top: 60px;
+}
+
+.head-user {
+    display: flex;
+    align-items: center;
+  .components-avatar {
+    width: 120px;
+    height: 120px;
+    border-radius: 8px;
+  }
+}
+
+.head-user__info {
+  height: 120px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 20px;
+
+  h2 {
+    padding: 0;
+    margin: 0;
+    font-size:48px;
+    font-weight:600;
+    color:rgba(255,255,255,1);
+    line-height:67px;
+  }
+
+  .dao__info__number {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size:20px;
+    font-weight:500;
+    color:rgba(255,255,255,1);
+    line-height:28px;
+
+    .dao__info__number__block {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+      margin-left: 26px;
+      &:nth-child(1) {
+        margin-left: 0;
+      }
+    }
+    .icon {
+      font-size: 30px;
+    }
+  }
+}
+
+.head-amount {
+  font-size: 20px;
+  font-weight: 500;
+  color: rgba(255,255,255,1);
+  line-height: 28px;
+}
+
+
+
+.token-brief {
+  padding: 20px 0 0;
+  margin: 0;
+  font-size:20px;
+  font-weight:400;
+  color:rgba(255,255,255,1);
+  line-height:30px;
+}
 </style>
