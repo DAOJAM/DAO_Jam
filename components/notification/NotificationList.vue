5dde3d1d82d8ed31e8967cea0219ad5cf4f3e955
xiaotian <952822399@qq.com>
2020-03-18 15:42:44 +0800

    粉丝列表

--- components/notification/NotificationList.vue
+++ components/notification/NotificationList.vue
@@ -39,59 +39,67 @@ export default {
 }
 </script>
 <style lang="less" scoped>
-  .card{
-    height: 80px;
-    border-radius: 10px;
-    padding: 20px;
-    margin: 0 0 20px 20px;
-    box-sizing: border-box;
-    background: rgba(255,255,255,1);
-    position: relative;
-    /deep/ .soruce-avatar{
-      position: absolute;
-      top: 20px;
-      left: 20px;
+.card {
+  height: 80px;
+  border-radius: 10px;
+  padding: 20px;
+  margin: 0 0 20px 20px;
+  box-sizing: border-box;
+  background: #132d5e;
+  position: relative;
+  /deep/ .soruce-avatar {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  /deep/ .message {
+    margin: 0 0 20px 60px;
+    font-size: 16px;
+    color: #b2b2b2;
+    p {
+      margin: 0;
+      padding: 0;
+      font-size: 14px;
+      font-weight: 400;
+      color: rgba(178, 178, 178, 1);
+      line-height: 20px;
     }
-    /deep/ .message{
-      margin: 0 0 20px 60px;
+    b {
       font-size: 16px;
-      color: #B2B2B2;
-      p{
-        margin: 0;
-        line-height: 22px;
-      }
-      b{
-        color: #000;
-      }
-      time{
-        font-size: 14px;
-        line-height: 20px;
-      }
+      color: rgba(255, 255, 255, 1);
+      line-height: 22px;
     }
-    /deep/ .action{
-      position: absolute;
-      top: 25px;
-      right: 20px;
-      button{
-        width: 80px;
-        height: 30px;
-        padding: 0;
-        border-radius: 6px;
-        background: rgba(0,0,0,1);
-        font-weight: 500;
-        color: rgba(255,255,255,1);
+    time {
+      font-size: 14px;
+      line-height: 20px;
+      color: #b2b2b2;
+    }
+  }
+  /deep/ .action {
+    position: absolute;
+    top: 25px;
+    right: 20px;
+    button {
+      width: 80px;
+      height: 30px;
+      padding: 0;
+      border-radius: 6px;
+      background: #6236FF;
+      font-weight: 500;
+      border-color: #6236FF;
+      color:  #fff;
+      line-height: 20px;
+      letter-spacing: 1px;
+      svg {
+        width: 12px;
+      }
+      &:disabled {
+        background: transparent;
+        border: 1px solid #6236FF;
+        color: #6236FF;
         line-height: 20px;
-        letter-spacing: 1px;
-        svg{
-          width: 12px;
-        }
-        &:disabled{
-          background: transparent;
-          border: 1px solid rgba(0,0,0,1);
-          color: rgba(0,0,0,1);
-          line-height: 20px;
-        }
       }
     }
   }
+}
 </style>
