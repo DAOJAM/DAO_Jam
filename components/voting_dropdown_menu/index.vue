c01a5cbe6830520040954a3af11c999753c9c88a
shellteo <shellteo@163.com>
2020-04-16 16:02:16 +0800

    update introduction

--- components/voting_dropdown_menu/index.vue
+++ components/voting_dropdown_menu/index.vue
@@ -321,16 +321,14 @@ export default {
     showVotingPowerIntroduction() {
       const h = this.$createElement
       this.$msgbox({
-        title: '介绍',
+        title: 'Introduction',
         message: h('div', null, [
-          h('h2', null, 'Voting Power是什么？'),
-          h('p', null, 'Voting Power是DAOJAM中用来支持项目的一种资源，每一个Voting Power都是 ERC-20 的Token。在DAOJAM中采用了二次方投票法：当您希望给一个项目支持5票时，就需要付出 5^2 = 25 个Voting Power。最终得票最高的项目会赢得DAOJAM的项目大奖，而在得奖项目中投Voting Power最多的用户会获得伯乐奖励。您的每一次投票都会被记录在区块链上，保障DAOJAM比赛过程中的公开透明性。'),
-          h('h2', null, '如何获得票？'),
-          h('p', null, '绑定Github和Email可自动领取 100 Voting Power'),
-          h('p', null, '每天可在导航栏中手动领取昨日投Voting Power总量的1/3 + 20 Voting Power'),
-          h('p', null, '邀请一名新用户领取奖励，自己可自动领取 50 Voting Power'),
-          h('h2', null, '如何投票？'),
-          h('p', null, '前往PROJECTS页面查看感兴趣的项目，点击项目可以进入详情页中查看。对于支持的项目可以在详情页中直接投票。相信您的眼光，一定可以为我们找到最优质的的好项目！不过还请注意：单个项目单日最多可投票3次 且 投票总量不超过 10 票。')
+          h('h2', null, 'What is Voting Power?'),
+          h('p', null, 'Voting Power is a resource used in DAO JAM to support your favorite hackathon projects. Quadratic voting is adopted in DAO Jam: when you want to submit 5 votes to a project, you need to pay 5 ^ 2 = 25 Voting Power. At the end of the hackathon, the project with the highest number of votes will win. Each of your votes will be recorded on the blockchain to ensure transparency and immutability during the hackathon.'),
+          h('h2', null, 'How Can I Get More Voting Power?'),
+          h('p', null, 'Link you NEAR Wallet account! '),
+          h('h2', null, 'How to vote?'),
+          h('p', null, 'Go to the PROJECTS page to view a list of hackathon project. After you enter the details page of each project, you can vote for you favorite projects!')
         ])
       })
     }
--- pages/account/index.vue
+++ pages/account/index.vue
@@ -1,30 +1,6 @@
 <template>
   <userLayout>
     <template slot="main">
-      <!-- <div class="cny-main">
-        <h2>
-          Voting Power是什么？
-        </h2>
-        <p>
-          Voting Power是DAOJAM中用来支持项目的一种资源，每一个Voting Power都是 ERC-20 的Token。在DAOJAM中采用了二次方投票法：当您希望给一个项目支持5票时，就需要付出 5^2 = 25 个 Voting Power。最终得票最高的项目会赢得DAOJAM的项目大奖，而在得奖项目中投Voting Power最多的用户会获得伯乐奖励。您的每一次投票都会被记录在区块链上，保障DAOJAM比赛过程中的公开透明性。
-        </p>
-        <h2>
-          如何获得票？
-        </h2>
-        <p>
-          绑定Github和Email可自动领取 100 Voting Power
-          <br>
-          每天可在导航栏中手动领取昨日投Voting Power总量的1/3 + 20 Voting Power
-          <br>
-          邀请一名新用户领取奖励，自己可自动领取 50 Voting Power
-        </p>
-        <h2>
-          如何投票？
-        </h2>
-        <p class="no-bottom">
-          前往PROJECTS页面查看感兴趣的项目，点击项目可以进入详情页中查看。对于支持的项目可以在详情页中直接投票。相信您的眼光，一定可以为我们找到最优质的的好项目！不过还请注意：单个项目单日最多可投票3次 且 投票总量不超过 10 票。
-        </p>
-      </div> -->
       <points />
     </template>
     <template slot="nav">
